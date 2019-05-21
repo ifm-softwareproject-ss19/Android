e0d78ed80b3ba5e257aa80dcaa96ec15b2c465e9
unknown <tissen_jakob@web.de>
2019-05-22 00:43:33 +0200

    Mockup umsetzung version 1

---
+++ test/app/src/main/java/test/h/VerbindungenActivity.java
@@ -0,0 +1,13 @@
+package test.h;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class VerbindungenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verbindungen);
+    }
+}
